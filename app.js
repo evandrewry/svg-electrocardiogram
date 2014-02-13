14601123346fdb821437020bf6bedaa605df94cc
Evan Drewry <evandrewry@gmail.com>
2014-02-13 12:05:55 -0500

    try to make faster?

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ angular.module('ekg', ['ng'])
 
 .controller('EkgCtrl', function () {
   return {
-    paths: new Array(20),
-    inverse5: function (n) {
-      return 1 / Math.pow(n, 5)
-    }
+    paths: new Array(5),
   };
 })
 
